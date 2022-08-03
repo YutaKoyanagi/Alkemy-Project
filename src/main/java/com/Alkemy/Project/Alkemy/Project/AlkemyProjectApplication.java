6d7546a6d0e9c1d5457d7431c4e43fa341b3f48b
Yuta Koyanagi <yutakoyanagi13@gmail.com>
2022-08-03 10:37:24 -0300

    initial commit

---
+++ src/main/java/com/Alkemy/Project/Alkemy/Project/AlkemyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Alkemy.Project.Alkemy.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkemyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkemyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Alkemy/Project/Alkemy/Project/AlkemyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alkemy.Project.Alkemy.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
